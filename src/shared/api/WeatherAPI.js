687ec4c48a9091ba8afcfd01b8d815c2f3f943bb
Stefan Garces <stefangarces@gmail.com>
2021-12-01 21:08:31 +0100

    added a counter to Sell view

--- src/shared/api/WeatherAPI.js
+++ src/shared/api/WeatherAPI.js
@@ -1,25 +1,3 @@
-/*
-export async function GetWeather() {
-    let cityName = "Gothenburg"
-    let response = await fetch(                                                                             // The API key
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=en&appid=34832f1e903a4e490cfc9a2d3fffea23`
-    );
-
-    if (response.ok) {
-        let json = await response.json();
-        let weather = json.weather[0].description;
-        let temp = json.main.temp;
-
-        return (
-            <h1>{temp}</h1>,
-            <h1>{weather}</h1>
-        )
-    } else {
-        alert("Couldn't find the weather of that city.");
-    }
-}
-*/
-
 import axios from "axios";
 
 const WeatherAPI = axios.create({
--- src/views/sell/SellObjectView.js
+++ src/views/sell/SellObjectView.js
@@ -1,8 +1,12 @@
+import React, {useState} from "react";
+
 export const SellObjectView = () => {
+    const [count, setCount] = useState(0)
 
     return (
         <div>
-            <h1>Sell Object</h1>
+            <h1>Count to {count}/100</h1>
+            <button onClick={()=> setCount(count + 2)}>CLICK ME</button>
         </div>
     )
 }
